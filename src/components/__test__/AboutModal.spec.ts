4e9b116e0d0e3967e9daa146234ee8ad216cc1e6
psi <cy.lonsido@gmail.com>
2022-03-08 14:47:30 +0800

    test boilerplate

---
+++ src/components/__test__/AboutModal.spec.ts
@@ -0,0 +1,56 @@
+import AboutModal from '@/components/AboutModal.vue'
+import { mount, VueWrapper } from '@vue/test-utils'
+import { mergeWith } from 'lodash'
+import {
+  IonContent,
+  IonButton,
+  IonButtons,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue'
+
+
+const createWrapper = (overrides?: any) => {
+  const defaultMountingOptions = {
+    global: {
+      components: {
+        IonContent,
+        IonButton,
+        IonButtons,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+      }
+    }
+  }
+
+  return mount(
+    AboutModal,
+    mergeWith(defaultMountingOptions, overrides)
+  )
+}
+
+describe(
+  'AboutModal unit test',
+  () => {
+    let wrapper: VueWrapper<any>
+
+    beforeEach(() => {
+      wrapper = createWrapper()
+    })
+
+    afterEach(() => {
+      if (wrapper) wrapper.unmount()
+
+      vi.clearAllMocks()
+    })
+
+    test(
+      'sanity check',
+      () => {
+        console.log(wrapper)
+      }
+    )
+  }
+)
