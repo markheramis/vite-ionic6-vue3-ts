e006b462041e7066bc45bdadba71748fec7c4f79
psi <cy.lonsido@gmail.com>
2022-03-08 10:34:09 +0800

    create the views

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+import {
+  IonContent,
+  IonButton,
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue'
+</script>
+
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Ionic Vite - Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class='ion-padding'>
+      <h1>Home</h1>
+      <ion-button router-link='/about'>About</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
